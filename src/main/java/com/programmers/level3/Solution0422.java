b7ba9d7a5315260110c7b464e6aef1796b830210
hyeeuncho <heeuncho@naver.com>
2026-04-22 10:18:34 +0900

    20260422 - 숫자 문자열과 영단어

---
+++ src/main/java/com/programmers/level3/Solution0422.java
@@ -0,0 +1,14 @@
+package com.programmers.level3;
+
+
+public class Solution0422 {
+    public int solution(String s) {
+        String[] words = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+        for (int i = 0; i < words.length; i++){
+            s = s.replace(words[i], String.valueOf(i));
+        }
+
+        return Integer.parseInt(s);
+    }
+}
