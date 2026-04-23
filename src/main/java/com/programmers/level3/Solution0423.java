b67f516830562d7c8fa81a97364e0d643b95a71f
hyeeuncho <heeuncho@naver.com>
2026-04-23 10:28:03 +0900

    20260423 - 문자열 내맘대로 정렬하기

---
+++ src/main/java/com/programmers/level3/Solution0423.java
@@ -0,0 +1,15 @@
+package com.programmers.level3;
+
+import java.util.Arrays;
+
+public class Solution0423 {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, (a,b) -> {
+            if (a.charAt(n) == b.charAt(n)){
+                return a.compareTo(b);
+            }
+            return a.charAt(n) - b.charAt(n);
+        });
+        return strings;
+    }
+}
